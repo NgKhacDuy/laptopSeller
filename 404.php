ad90314f4b25598ffead116f8065943a52792a8e
khacduy <2002nguyenkhacduy@gmail.com>
2023-05-12 19:45:54 +0700

    KDuy

--- 404.php
+++ 404.php
@@ -1,3 +1,16 @@
-
-	<h1>405 Not Found</h1>
-	<p>The page you are looking for could not be found.</p>
+<?php
+    include 'inc/header.php'
+?>
+	<div class="d-flex align-items-center justify-content-center vh-100">
+            <div class="text-center">
+                <h1 class="display-1 fw-bold">404</h1>
+                <p class="fs-3"> <span class="text-danger">Opps!</span> Không tìm thấy trang.</p>
+                <p class="lead">
+                    Trang bạn tìm kiếm không tồn tại.
+                  </p>
+                <a href="trangchu" class="btn btn-primary">Về trang chủ</a>
+            </div>
+        </div>
+<?php
+    include 'inc/footer.php'
+?>
